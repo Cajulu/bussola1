f05a4762afc9e6c490acc148e0aec45682c97b75
luana <luanabgn@live.com>
2017-05-15 16:05:11 -0300

    att

--- bussola/models.py
+++ bussola/models.py
@@ -3,37 +3,70 @@ from django.utils import timezone
 
 class Notificacao(models.Model):
 	descricao = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.descricao
 	
 class Cidade(models.Model):
 	descricao = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.descricao
 
 class Servico(models.Model):
 	preco = models.DecimalField(max_digits=5,decimal_places=2,null=False)
 	nome = models.CharField(max_length=45)
 	informacoes_adicionais = models.CharField(max_length=200)
 
+	def __str__(self):
+		return self.preco, self.nome, self.informacoes_adicionais
+
+
 class Sub_categoria(models.Model):
 	descricao = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.descricao
+
 
 class Comentario_servico(models.Model):
 	comenta_ser =  models.CharField(max_length=200)
 
+	def __str__(self):
+		return self.comenta_ser
+
 class Tipo_contato(models.Model):
 	tipo = models.CharField(max_length=45)
+
+	def __str__(self):
+		return self.tipo
+
 
 class Contato(models.Model):
 	tipo_contato = models.ForeignKey(Tipo_contato)
 	descricao = models.CharField(max_length=45)
 	
+	def __str__(self):
+		return self.descricao
+
 class Avaliacao_servico(models.Model):
 	numero_estrelas_ser = models.IntegerField()
+
+	def __str__(self):
+		return self.numero_estrelas_ser
 
 class Categoria(models.Model):
 	sub_categoria = models.ForeignKey(Sub_categoria)
 	descricao = models.CharField(max_length=45)
 
+	def __str__(self):
+		return self.descricao
+
 class Estabelecimeto(models.Model):
 	nome = models.CharField(max_length=45)
+
+	def __str__(self):
+		return self.nome
 
 class Usuario(models.Model):
 	cpf_cnpj = models.IntegerField()
@@ -42,22 +75,45 @@ class Usuario(models.Model):
 	nome = models.CharField(max_length=45)
 	foto = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
 
+	def __str__(self):
+		return self.cpf_cnpj,self.senha,self.email,self.nome,self.foto
+
 class Endereco (models.Model):
 	complemento = models.CharField(max_length=45)
 	bairro = models.CharField(max_length=45)
 	numero  = models.IntegerField()
 	rua = models.CharField(max_length=45)
 
+	def __str__(self):
+		return self.complemento,self.bairro,self.numero,self.rua
+
 class Avaliacao_estabelecimento(models.Model):
 	numero_estrelas_est = models.IntegerField()
 
+	def __str__(self):
+		return self.numero_estrelas_est
+
 class Comentario_estabelecimento(models.Model):
 	comenta_est =  models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.comenta_est
 
 class Imagens(models.Model):
 	servico = models.ForeignKey(Servico)
 	descricao_imagem = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
 
+	def __str__(self):
+		return self.descricao_imagem
+
+
+class Fale_conosco(models.Model):
+	email = models.EmailField(max_length=254)
+	mensagem = models.CharField(max_length=200)
+
+	def __str__ (self):
+		return self.email, self.mensagem
+		
 class Usuario_le_notificacao(models.Model):
 	notificacao = models.ForeignKey(Notificacao)
 	usuario = models.ForeignKey(Usuario)
@@ -121,7 +177,3 @@ class Avalia_local(models.Model):
 class Usuario_cidade(models.Model):
 	endereco = models.ForeignKey(Endereco)
 	cidade = models.ForeignKey(Cidade)
-
-class fale_conosco(models.Model):
-    email = models.EmailField(max_length=254)
-    mensagem = models.CharField(max_length=200)
